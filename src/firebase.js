364dba27e761c68d5a5e26837b084c5baf81dcfa
Noble-Knight-7 <83090196+Noble-Knight-7@users.noreply.github.com>
2026-03-28 21:14:52 +0600

    add env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWTshbMZVJCOskOgfuru1UpQLCju3_4I8",
-  authDomain: "quran-connect-4357d.firebaseapp.com",
-  projectId: "quran-connect-4357d",
-  storageBucket: "quran-connect-4357d.firebasestorage.app",
-  messagingSenderId: "403211475863",
-  appId: "1:403211475863:web:6361434619176cd2a002d2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// These are the two services we'll use
-export const db = getFirestore(app); // database
-export const auth = getAuth(app); // login system
+export const db = getFirestore(app);
+export const auth = getAuth(app);
